da93018e0c5762c6c33e7643966e764896864162
Jonny Holupchinski <jonholup@gmail.com>
2017-02-13 14:05:32 -0600

    functions module complete

--- 6-functions/func.js
+++ 6-functions/func.js
@@ -1,13 +1,20 @@
 // 1 - Write a function declaration
 // that takes no arguments and
 // returns the value 4
-
+function four () {
+  return 4;
+}
 // 2 - Write a function declaration
 // that takes two arguments and
 // returns the value of the result
 // of the multiplication of both
 // arguments
-
+function multiply (a, b) {
+  return a * b;
+}
 // 3 - Write a function expression
 // that takes one argument and
 // console logs that value.
+var funcExpression = function (a) {
+  console.log(a);
+}
