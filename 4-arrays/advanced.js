a91a28b1732e0d86fe512ab52c69aba7dfb220ad
Jonny Holupchinski <jonholup@gmail.com>
2017-02-13 13:50:58 -0600

    arrays module complete

--- 4-arrays/advanced.js
+++ 4-arrays/advanced.js
@@ -3,9 +3,13 @@
 // the following array.
 
 var arr = [3, 4, 6, 0];
-
+arr.push(32);
+console.log(arr);
 // 2 - Remove the first value from
 // arr using the Array built-in method slice.
-
+arr = arr.slice(1);
+console.log(arr);
 
 // 3 - Add the number 11 to the beginning of arr
+arr.unshift(11);
+console.log(arr);
